8426d0293a3489e07aff4d9026fb44bea6ba7437
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-15 15:17:01 +0100

    Networking Server code

--- src/networking/GreetServer.java
+++ src/networking/GreetServer.java
@@ -1,2 +1,35 @@
-package networking;public class GreetServer {
+ package networking;
+
+import java.net.*;
+import java.io.*;
+import java.io.IOException;
+
+public class GreetServer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        System.out.println(greeting);
+            out.println("hello Iseoluwa");
+
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) throws IOException {
+        GreetServer server=new GreetServer();
+        server.start(6666);
+    }
 }
