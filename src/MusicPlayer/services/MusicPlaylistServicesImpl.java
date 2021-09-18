5f51914f425353481255b3b09a2a1ce9fc14d8da
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-18 06:35:24 +0100

    Music Playlist (service) Impl

--- src/MusicPlayer/services/MusicPlaylistServicesImpl.java
+++ src/MusicPlayer/services/MusicPlaylistServicesImpl.java
@@ -1,2 +1,37 @@
-package MusicPlayer.services;public class MusicPlaylistServicesImpl {
+package MusicPlayer.services;
+
+import MusicPlayer.entity.Music;
+import MusicPlayer.repository.PlaylistDB;
+
+public class MusicPlaylistServicesImpl  implements MusicPlaylistServices{
+
+    private PlaylistDB playlistDB;
+
+    public MusicPlaylistServicesImpl() {
+        playlistDB = PlaylistDB.getInstance();
+    }
+
+    @Override
+    public void addToPlaylist(Music songTitle) {
+            playlistDB.download(songTitle);
+    }
+
+    @Override
+    public void delete(Music songTitle) {
+        playlistDB.deleteMusic(songTitle);
+    }
+
+    @Override
+    public int getNumberOfMusic() {
+        return playlistDB.size();
+    }
+
+    @Override
+    public Music search(Music songTitle) {
+        return playlistDB.searchFor(songTitle);
+    }
+
+    public void clearDB(){
+        playlistDB.clearDb();
+    }
 }
