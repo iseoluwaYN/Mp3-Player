aa6473b5550c277c56de8cb216e6372dfe7ba53a
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-18 06:29:53 +0100

    Album

--- src/MusicPlayer/entity/Album.java
+++ src/MusicPlayer/entity/Album.java
@@ -1,2 +1,17 @@
-package MusicPlayer.entity;public class Album {
+package MusicPlayer.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    public String name;
+    List<Music> music = new ArrayList<Music>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
