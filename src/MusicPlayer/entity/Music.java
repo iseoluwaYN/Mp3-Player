18fe6259a64ccf9eb4f35df3a855b299de3a41cf
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-18 06:31:16 +0100

    Music

--- src/MusicPlayer/entity/Music.java
+++ src/MusicPlayer/entity/Music.java
@@ -1,2 +1,13 @@
-package MusicPlayer.entity;public class Music {
+package MusicPlayer.entity;
+
+public class Music {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
