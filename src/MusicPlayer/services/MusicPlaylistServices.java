cc8170e957179d535c67dfd191c81de254c12d23
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-09-18 06:34:55 +0100

    Music Playlist (service)

--- src/MusicPlayer/services/MusicPlaylistServices.java
+++ src/MusicPlayer/services/MusicPlaylistServices.java
@@ -1,6 +1,13 @@
 package MusicPlayer.services;
 
-public interface MusicPlaylist {
+import MusicPlayer.entity.Music;
 
-    
+public interface MusicPlaylistServices {
+
+    public void addToPlaylist(Music songTitle);
+    public void delete(Music songTitle);
+    public int getNumberOfMusic();
+    public Music search(Music songTitle);
+
+    void clearDB();
 }
